0f3b3c5221af4a60636c9e4f9b0b6a58534c6694
jmeaster30 <jhneasterday09@gmail.com>
2023-02-28 00:43:31 -0500

    Wrapped up base of demo

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const pics = [
     width: 1076, //main pic width in pixels
     height: 1356, //main pic height in pixels
     scale: 0.6,
-    file: "./imgs/clinton.png",
+    file: "https://raw.githack.com/jmeaster30/spotify-demo/main/imgs/clinton.png",
     boundaries: [
       {
         name: "hands",
@@ -49,8 +49,8 @@ const pics = [
     name: "obama",
     width: 1440,
     height: 1738,
-    scale: 0.6,
-    file: "./imgs/obama.png",
+    scale: 0.5,
+    file: "https://raw.githack.com/jmeaster30/spotify-demo/main/imgs/obama.png",
     boundaries: [
       {
         name: "obama",
@@ -67,17 +67,168 @@ const pics = [
 
 let mainPicture = null
 let selectedModel = null
+let selectedBoundary = null
+let boundaryImage = {}
+let saveImage = false
+let imageFilename = 'coolimageXD'
+
+function handleSelectBoundary(event) {
+  selectedBoundary = event.target.value
+}
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+  }
+}
+
+async function doSearch() {
+  let searchBox = document.getElementById('searchText');
+  let searchResults = document.getElementById('results');
+  removeAllChildNodes(searchResults);
+  let searchText = searchBox.value;
+  console.log("searching", searchText);
+  let access_token = null;
+  await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    body: 'grant_type=client_credentials',
+    headers: {
+      'Authorization': `Basic ${btoa('f01b9f12b0964f3bbbca7c17c417deb1:55421359dc4d4c65ae2ac12f79f30524')}`,
+      "Content-type": "application/x-www-form-urlencoded",
+    }
+  }).then(async (result) => {
+    const data = await result.json();
+    access_token = data.access_token;
+  }).catch(err => console.error(err));
+
+  if (!access_token) {
+    return;
+  }
+
+  await fetch(`https://api.spotify.com/v1/search?q=${searchText}&type=album&limit=15`, {
+    headers: {
+      'Authorization': `Bearer ${access_token}`,
+      'Content-Type': 'application/json'
+    }
+  }).then(async (res) => {
+    const data = await res.json();
+    foundAlbums = data.albums?.items.map((value) => ({
+      printURL: value.images[0].url,
+      displayURL: value.images[value.images.length - 1].url,
+      albumName: value.name,
+      artists: value.artists.map((x) => x.name)
+    }));
+    
+    console.log(foundAlbums)
+
+    // load search results into view
+
+    for (let i = 0; i < foundAlbums.length; i++) {
+      let result = document.createElement('div');
+      result.className = "resultEntry"
+
+      result.addEventListener('click', function(){
+        console.log("CLICK");
+        boundaryImage[selectedBoundary] = loadImage(foundAlbums[i].printURL);
+      })
+
+      let resultImageSpan = document.createElement('div');
+      result.appendChild(resultImageSpan);
+
+      let resultOtherSpan = document.createElement('div');
+      result.appendChild(resultOtherSpan);
+
+      let resultImage = document.createElement('img');
+      resultImage.src = foundAlbums[i].displayURL;
+      resultImageSpan.appendChild(resultImage);
+      
+      let resultTitle = document.createElement('div');
+      resultTitle.className = "resultTitle"
+      resultTitle.innerHTML = foundAlbums[i].albumName
+      resultOtherSpan.appendChild(resultTitle);
+
+      let resultArtists = document.createElement('div');
+      resultArtists.className = "resultArtists";
+      for (let j = 0; j < foundAlbums[i].artists.length; j++) {
+        let resultArtist = document.createElement('span');
+        resultArtist.className = "resultArtist";
+        resultArtist.innerHTML = foundAlbums[i].artists[j];
+        resultArtists.appendChild(resultArtist)
+      }
+      resultOtherSpan.appendChild(resultArtists);
+
+      searchResults.appendChild(result)
+    }
+  })
+  .catch(err => console.error(err));
+}
+
+function doSaveImage() {
+  saveImage = true;
+}
+
+function doSetImageFilename() {
+  let filename = document.getElementById('imageFilename');
+  imageFilename = filename.value;
+}
+
+const debounce = (func, delay) => {
+  let debounceTimer;
+  return function() {
+    const context = this;
+    const args = arguments;
+    clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(() => func.apply(context, args), delay);
+  }
+}
 
 function preload() {
   selectedModel = pics[Math.floor(Math.random() * pics.length)]
   mainPicture = loadImage(selectedModel.file)
+  
+  selectedBoundary = selectedModel.boundaries[0].name
+  let select = document.getElementById('locations');
+  select.addEventListener('change', handleSelectBoundary);
+  for(let i = 0; i < selectedModel.boundaries.length; i++) {
+    let opt = selectedModel.boundaries[i].name;
+    let el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    select.appendChild(el);
+  }
+
+  let searchBox = document.getElementById('searchText');
+  searchBox.addEventListener('input', debounce(doSearch, 500));
+
+  document.getElementById('saveImageButton').addEventListener('click', doSaveImage);
+  document.getElementById('imageFilename').addEventListener('input', doSetImageFilename);
 }
 
 function setup() {
-  createCanvas(selectedModel.width * selectedModel.scale, selectedModel.height * selectedModel.scale);
+  let container = document.getElementById('p5container')
+  let desiredWidth = container.offsetWidth;
+  selectedModel.scale = desiredWidth / selectedModel.width;
+  let cnv = createCanvas(selectedModel.width * selectedModel.scale, selectedModel.height * selectedModel.scale, WEBGL);
+  cnv.parent('p5container')
 }
 
 function draw() {
   background(0);
+  translate(-width / 2, -height / 2);
+  for (let i = 0; i < selectedModel.boundaries.length; i++) {
+    let boundaryName = selectedModel.boundaries[i].name
+    if (boundaryImage[boundaryName]) {
+      noStroke();
+      texture(boundaryImage[boundaryName]);
+      quad(selectedModel.boundaries[i].quad.top_left.x * selectedModel.scale, selectedModel.boundaries[i].quad.top_left.y * selectedModel.scale,
+        selectedModel.boundaries[i].quad.top_right.x * selectedModel.scale, selectedModel.boundaries[i].quad.top_right.y * selectedModel.scale, 
+        selectedModel.boundaries[i].quad.bottom_right.x * selectedModel.scale, selectedModel.boundaries[i].quad.bottom_right.y * selectedModel.scale, 
+        selectedModel.boundaries[i].quad.bottom_left.x * selectedModel.scale, selectedModel.boundaries[i].quad.bottom_left.y * selectedModel.scale)
+    }
+  }
   image(mainPicture, 0, 0, selectedModel.width * selectedModel.scale, selectedModel.height * selectedModel.scale)
+  if (saveImage) {
+    saveCanvas(`${imageFilename}.png`);
+    saveImage = false;
+  }
 }
